04b5f17589aa6a9f22889da33d277a47f41a4de2
KingSea168 <qykingsea_168@163.com>
2018-01-06 08:50:01 +0800

    1.fix can not run project 2.add annother way to get one person info:person/persons/1

--- java/com/king/ssh/controller/PersonController.java
+++ java/com/king/ssh/controller/PersonController.java
@@ -6,7 +6,9 @@ import java.util.Objects;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.king.ssh.entity.Person;
@@ -44,6 +46,17 @@ public class PersonController {
 	@RequestMapping(value="/getPersonById")
 	@ResponseBody
 	public Person getPersonById(String id){
+		return personService.getPersonById(id);
+	}
+	
+	/**
+	 * 增加PathVariable路径请求方式获取某个人员信息
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value="persons/{id}",method=RequestMethod.GET)
+	@ResponseBody
+	public Person getPerson(@PathVariable("id")String id){
 		return personService.getPersonById(id);
 	}
 	
